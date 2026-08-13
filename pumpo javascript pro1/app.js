2248d7004d7514f7ab2e10d9d58bacbe1f3586b4
Saravanan -M <150891045+pulsar05@users.noreply.github.com>
2026-08-13 13:55:15 +0530

    Add files via upload

--- pumpo javascript pro1/app.js
+++ pumpo javascript pro1/app.js
@@ -1,249 +1,446 @@
 // 
 
-// task 1 student details
+// // task 1 student details
 
-const college = "Suguna college of engineering"
-console.log(college);
+// const college = "Suguna college of engineering"
+// console.log(college);
 
-let studentName = "Saravanan M"
-console.log(studentName);
+// let studentName = "Saravanan M"
+// console.log(studentName);
 
-var age = 27
-console.log(age);
+// var age = 27
+// console.log(age);
 
-// task 2 mobile price
+// // task 2 mobile price
 
-let mobilePrice = 25000
-let change = mobilePrice = 27000
-console.log(mobilePrice); 
+// let mobilePrice = 25000
+// let change = mobilePrice = 27000
+// console.log(mobilePrice); 
 
 
-// task3 company name
-function button(){
-    const companyName = document.getElementById("input").value;
+// // task3 company name
+// function button(){
+//     const companyName = document.getElementById("input").value;
 
-if (companyName == "Stackly"){
-      console.log("Access Granted");
+// if (companyName == "Stackly"){
+//       console.log("Access Granted");
 
+// }
+// else{
+//     alert("check the company Name")
+// }
+// }
+
+// // task 4 console practice
+//  const login = "login successfully"
+//  console.log(login);
+//  const password = "password is weak"
+//  console.warn(password);
+//  const network = "network error"
+//  console.error(network);
+
+// //  task 5 data type
+
+// const name = "saravanan M"
+// console.log(typeof(name))
+
+// const ages = 27;
+// console.log(typeof ages);
+
+// const isStudent = true
+// console.log(typeof(isStudent))
+
+// // task shopping card
+
+// const shoppingCard = ["laptop" , "mouse" , "keyboard" , "headset" , "charger"]
+// console.log(shoppingCard[1]);
+// console.log(shoppingCard[2]);
+// console.log(shoppingCard[4]);
+
+// // task 7 movies name
+// const movieName = ["gilli" , "kgf" , "mersal" , "goat" , "leo" , "vikaram","sarkar" , "beast"]
+// console.log(movieName[4]);
+// console.log(movieName[6]);
+// console.log(movieName[7]);
+
+// // task 8 employee object
+
+// const employee = {
+//     name: "saravanan M",
+//     age: 27,
+//     department: "software developer",
+//     skills: "array",
+//     salery: 25000
+// }
+// console.log(employee.name);
+// console.log(employee.department);
+// console.log(employee.skills);
+// console.log(employee.salery);
+
+// // task 9 product details
+
+// const employees = {
+//     productName: "shoes ",
+//     brandName: "Adidas",
+//     price: 5000,
+//     color: "white",
+//     secondColor:"black"
+    
+// }
+// console.log(employees.productName);
+// console.log(employees.brandName);
+// console.log(employees.price);
+// console.log(employees.secondColor);
+
+
+// // task 10 arithamatic operations
+
+// const num1 = 10;
+// const num2 = 5;
+
+// console.log("Additon:" , num1 +num2);
+// console.log("subtraction:" , num1 - num2);
+// console.log("multi:" , num1 * num2);
+// console.log("division:" , num1 / num2);
+// console.log("modulus:" , num1 % num2);
+// console.log("explosion:" , num1 ** num2);
+
+// // task 11 increment practice
+
+// let a = 5;
+
+// console.log(a++);
+// console.log(++a);
+// console.log(a--);
+// console.log(--a);
+
+// // task 12 prdict the output
+
+// let a1 = 10;
+// let b = a1++;
+// let c = ++b;
+
+// console.log(a1);
+// console.log(b);
+// console.log(c);
+
+
+// // task13 predict the output
+
+
+// let x =7;
+// let y = --x;
+// let z = y++;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+// // task14 school object
+
+// const  schoolDetails = {
+
+//     schoolName: "SES matriculation higher secondary school",
+//     principal:"Rameskumar",
+//     totalStudents: 1000,
+//     classesArray:["1st standard" , "2nd standard" , "3rd standard"]
+// }
+// console.log(schoolDetails.schoolName);
+// console.log(schoolDetails.principal);
+// console.log(schoolDetails.classesArray[2]);
+
+
+// // task15 grocery list
+
+// const groceryItems = ["rice", "sugar" , "salt" , "oil" , "milk" , "egg" , "vegetable" , "apple"]
+
+// console.log(groceryItems[0],groceryItems[3] , groceryItems[7]);
+
+
+// // rask 16 user profile
+
+// const userProfile = {
+
+//     username: "saravanan M",
+//     emailId: "manojchithar05@gmail.com",
+//     age: 27,
+//     IsPremium:true
+// }
+// console.log(typeof(userProfile.username),userProfile.username);
+// console.log(typeof(userProfile.emailId),userProfile.emailId);
+// console.log(typeof(userProfile.age),userProfile.age);
+// console.log(typeof(userProfile.IsPremium),userProfile.IsPremium);
+
+
+// // task17 varaibale rules
+
+// var firstName = "saravanan";
+
+// var firstName = "saravanan M"
+// console.log(firstName);
+
+// // let lastName = "M";
+
+// // let lastName = "Murugesan"
+// // console.log(lastName);  this will give the error 
+
+// // const fullName = "saravanan M"
+// //  const fullName = "Saravanan Murugesan"
+// // console.log(fullName); this also give the error cannot resign
+
+
+// // task 18 marks calculation
+
+// let tamil = 85;
+// let english = 90;
+// let maths = 95;
+
+// let totalMarks = tamil + english + maths;
+//  let Average = totalMarks /3;
+
+//  console.log("totalMarks:" , totalMarks);
+//  console.log('Average:' , Average);
+
+
+// //  task 19 company database
+
+// const database = {
+
+//     companyName:  "stackly",
+//     EmployeName:["saravanaan " , "akshay" , "vikas" , "naveen" , "christo" , "vicky"],
+//     locaton:"coimbatore",
+//     foundedYear: 2020,
+//     CEO:"Manoj"
+// }
+// console.log(database.CEO);
+// console.log(database.location);
+// console.log(database.EmployeName[0]);
+
+
+// // task 20 mini profile
+
+// const miniProfile = {
+    
+
+//     name: "saravanan M",
+//     city: "coimbatore",
+//     hobbies: ["cricket", "reading books", "coding"],
+//     age: 27
+// }
+// console.log(miniProfile.name);
+// console.log(miniProfile.hobbies[0], miniProfile.hobbies[1], miniProfile.hobbies[2]);
+// console.log(miniProfile.age);
+
+
+// task 21 varaible student details
+
+const studentDetails = {
+       name: "saravanan M",
+       age: 27,
+       course: "python full stack course",
+       mark: 75
+
+}
+console.log(studentDetails.name)
+console.log(studentDetails.age)
+console.log(studentDetails.course)
+console.log(studentDetails.mark)
+
+// task 22 user input
+
+const userName = prompt("Enter the name");
+const userAge = prompt("Enter the age");
+console.log("userName:" , userName);
+console.log("userAge:" ,userAge);
+
+
+// task 23 data types
+
+const string = "saravananM"
+const number = 27
+const boolean = true
+const undefinedMethod = undefined
+const nullValue = null
+
+console.log(typeof(string));
+console.log(typeof(number));
+console.log(typeof(boolean));
+console.log(typeof(undefinedMethod));
+console.log(typeof(nullValue));
+
+
+// task 24  array shopping list
+
+const  shooping = ["paint" , "brush" , "bucket" , "soap" ,"metal" , "wood","cement","sand"]
+
+console.log(shooping[0])
+console.log(shooping[4])
+console.log(shooping[7])
+console.log(shooping)
+
+// task 25 object employee
+
+const details = {
+
+    name : "gopal",
+    age : 30,
+    role: "software developer",
+    salary : 35000
+}
+console.log(details.name)
+console.log(details.age)
+console.log(details.role)
+console.log(details.salery)
+
+
+// task 26 arthimatic operadtion
+
+const productPrice = 500
+const quality  = 3
+
+console.log(productPrice * quality)
+console.log(`the discount is${productPrice % quality}`)
+// console.log(productPrice += quality,productPrice %= quality,`total price is ${productPrice},${quality}`)
+
+
+// task 27 comparsion operation
+
+const num1 = 25 > 20
+const num2 = 15 < 10
+const num3 = 50 == "50"
+const num4 = 50 === "50"
+const num5 = 100 != "100"
+const num6 = 100 !== "100"
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+
+
+// task 28 logical operation
+
+console.log(10 > 5 && 20 > 15 || 5 > 10);
+
+console.log(10 < 5 || 20 >= 20 && 5 == "5");
+
+console.log(15 === "15" || 10 > 5 && 8 < 3);
+
+console.log(20 >= 20 && 5 !== "5" || 10 < 5);
+
+console.log(25 < 20 || 30 == "30" && 10 >= 10);
+
+// task 29 ternary operation
+
+let password = true;
+
+if (password == true){
+    console.log("login successfully")
 }
 else{
-    alert("check the company Name")
+    false
 }
+
+
+// task 30 type caasting 
+
+let mark1 = 80
+let mark2 = 70
+
+console.log(mark1 + mark2)
+
+// task 31 voting eligible 
+
+let age = 28
+
+
+if (age >= 28){
+
+    console.log("your are eligible to vote")
+} 
+else{
+    console.log("your are not eligible")
 }
 
-// task 4 console practice
- const login = "login successfully"
- console.log(login);
- const password = "password is weak"
- console.warn(password);
- const network = "network error"
- console.error(network);
+// task 32 student grade
 
-//  task 5 data type
+let mark = 85;
 
-const name = "saravanan M"
-console.log(typeof(name))
-
-const ages = 27;
-console.log(typeof ages);
-
-const isStudent = true
-console.log(typeof(isStudent))
-
-// task shopping card
-
-const shoppingCard = ["laptop" , "mouse" , "keyboard" , "headset" , "charger"]
-console.log(shoppingCard[1]);
-console.log(shoppingCard[2]);
-console.log(shoppingCard[4]);
-
-// task 7 movies name
-const movieName = ["gilli" , "kgf" , "mersal" , "goat" , "leo" , "vikaram","sarkar" , "beast"]
-console.log(movieName[4]);
-console.log(movieName[6]);
-console.log(movieName[7]);
-
-// task 8 employee object
-
-const employee = {
-    name: "saravanan M",
-    age: 27,
-    department: "software developer",
-    skills: "array",
-    salery: 25000
+if (mark >= 90 && mark <= 100) {
+    console.log("Grade-A");
 }
-console.log(employee.name);
-console.log(employee.department);
-console.log(employee.skills);
-console.log(employee.salery);
-
-// task 9 product details
-
-const employees = {
-    productName: "shoes ",
-    brandName: "Adidas",
-    price: 5000,
-    color: "white",
-    secondColor:"black"
-    
+else if (mark >= 75 && mark <= 89) {
+    console.log("Grade-B");
 }
-console.log(employees.productName);
-console.log(employees.brandName);
-console.log(employees.price);
-console.log(employees.secondColor);
-
-
-// task 10 arithamatic operations
-
-const num1 = 10;
-const num2 = 5;
-
-console.log("Additon:" , num1 +num2);
-console.log("subtraction:" , num1 - num2);
-console.log("multi:" , num1 * num2);
-console.log("division:" , num1 / num2);
-console.log("modulus:" , num1 % num2);
-console.log("explosion:" , num1 ** num2);
-
-// task 11 increment practice
-
-let a = 5;
-
-console.log(a++);
-console.log(++a);
-console.log(a--);
-console.log(--a);
-
-// task 12 prdict the output
-
-let a1 = 10;
-let b = a1++;
-let c = ++b;
-
-console.log(a1);
-console.log(b);
-console.log(c);
-
-
-// task13 predict the output
-
-
-let x =7;
-let y = --x;
-let z = y++;
-
-console.log(x);
-console.log(y);
-console.log(z);
-
-
-// task14 school object
-
-const  schoolDetails = {
-
-    schoolName: "SES matriculation higher secondary school",
-    principal:"Rameskumar",
-    totalStudents: 1000,
-    classesArray:["1st standard" , "2nd standard" , "3rd standard"]
+else if (mark >= 50 && mark <= 74) {
+    console.log("Grade-C");
 }
-console.log(schoolDetails.schoolName);
-console.log(schoolDetails.principal);
-console.log(schoolDetails.classesArray[2]);
-
-
-// task15 grocery list
-
-const groceryItems = ["rice", "sugar" , "salt" , "oil" , "milk" , "egg" , "vegetable" , "apple"]
-
-console.log(groceryItems[0],groceryItems[3] , groceryItems[7]);
-
-
-// rask 16 user profile
-
-const userProfile = {
-
-    username: "saravanan M",
-    emailId: "manojchithar05@gmail.com",
-    age: 27,
-    IsPremium:true
+else {
+    console.log("Fail");
 }
-console.log(typeof(userProfile.username),userProfile.username);
-console.log(typeof(userProfile.emailId),userProfile.emailId);
-console.log(typeof(userProfile.age),userProfile.age);
-console.log(typeof(userProfile.IsPremium),userProfile.IsPremium);
 
+// task 33 Time Greeting
 
-// task17 varaibale rules
+let time = 15;
 
-var firstName = "saravanan";
-
-var firstName = "saravanan M"
-console.log(firstName);
-
-// let lastName = "M";
-
-// let lastName = "Murugesan"
-// console.log(lastName);  this will give the error 
-
-// const fullName = "saravanan M"
-//  const fullName = "Saravanan Murugesan"
-// console.log(fullName); this also give the error cannot resign
-
-
-// task 18 marks calculation
-
-let tamil = 85;
-let english = 90;
-let maths = 95;
-
-let totalMarks = tamil + english + maths;
- let Average = totalMarks /3;
-
- console.log("totalMarks:" , totalMarks);
- console.log('Average:' , Average);
-
-
-//  task 19 company database
-
-const database = {
-
-    companyName:  "stackly",
-    EmployeName:["saravanaan " , "akshay" , "vikas" , "naveen" , "christo" , "vicky"],
-    locaton:"coimbatore",
-    foundedYear: 2020,
-    CEO:"Manoj"
+if (time >= 1 && time <= 6){
+    console.log("Early moring")
 }
-console.log(database.CEO);
-console.log(database.location);
-console.log(database.EmployeName[0]);
-
-
-// task 20 mini profile
-
-const miniProfile = {
-    
-
-    name: "saravanan M",
-    city: "coimbatore",
-    hobbies: ["cricket", "reading books", "coding"],
-    age: 27
+else if(time >= 7 && time <= 12){
+    console.log("good morning")
 }
-console.log(miniProfile.name);
-console.log(miniProfile.hobbies[0], miniProfile.hobbies[1], miniProfile.hobbies[2]);
-console.log(miniProfile.age);
+else if (time >= 13 && time <=15 ){
+    console.log("good Afternoon")
+}
+else if(time >= 16 && time <= 19){
+    console.log("Good evening")
+}
+else{
+    console.log("Good night")
+}
 
 
+// task 34  nested -job eleigible 
 
+let age = 25;
+let height = 175;
+let weight = 75;
 
+if (age >= 21) {
+    if (height >= 170) {
+        if (weight >= 70) {
+            console.log("Candidate is eligible");
+        } else {
+            console.log("Not eligible: Weight must be at least 70 kg");
+        }
+    } else {
+        console.log("Not eligible: Height must be at least 170 cm");
+    }
+} else {
+    console.log("Not eligible: Age must be at least 21");
+}
 
+// task 25 switch traffic method
 
+let trafficLight = "green";
 
+switch (trafficLight) {
+    case "red":
+        console.log("Stop the vehicle");
+        break;
 
+    case "yellow":
+        console.log("Get ready");
+        break;
 
+    case "green":
+        console.log("Go");
+        break;
 
-
-
-
-
+    default:
+        console.log("Invalid traffic light");
+}
 
 
